fdd137217b92c043cce863652787068da81cdcb3
3A24calixto <00001107065537sp@al.educacao.sp.gov.br>
2024-11-28 09:08:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Matematica',
+    'Qual é o valor dessa multiplicação 547x49',
+    '32.273'
+)
+
+criaCartao(
+    'Português',
+    'Quando surgiu o romantismo?',
+    'Seculo XVII'
+)
+
+criaCartao(
+    'Historia',
+    'Quando a guerra fria se iniciou?',
+    '12 de março de 1947'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Rússia?',
+    'Moscou'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual foi o maior fisico do tempo?',
+    'albert Einstein'
+)
+
+criaCartao(
+    'Matematica',
+    'Raiz quadrada de 1000?',
+    '31,62'
+)
